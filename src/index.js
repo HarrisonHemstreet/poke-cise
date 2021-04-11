366660a039475d730e35646d23d2a88f68d97e49
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-04-10 21:43:08 -0600

    added skeleton. Now to add the styling...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 
-const App = () {
+const App = () => {
+	const [pokedex, setPokedex] = useState([]);
+	const [wildPokemon, setWildPokemon] = useEffect({});
+
+	useEffect(() => {
+    	encounterWildPokemon();
+  		}, [pokedex]);
+
+    const pokeId = () => {
+		const min = Math.ceil(1);
+		const max = Math.floor(151);
+		return Math.floor(Math.random() * (max - min + 1)) + min
+	}
+
+	const encounterWildPokemon = () => {
+		axios
+	   		.get('https://pokeapi.co/api/v2/pokemon/' + pokeId())
+			.then(response => {
+				setWildPokemon(response.data);
+			})
+		}
+
+
   return (
     <div className="app-wrapper">
-    
-    </div>
+		<header>
+			<h1 className="title">Poke-Cise</h1>	
+			<h3 className="subtitle">With Pokemon using the PokeAPI!</h3>	
+		</header>
+		<section className="wild-pokemon">
+			<h2>Wild Encounter</h2>
+			<img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + wildPokemon.id + ".png"} className="sprite"/>
+			<h3>{wildPokemon.name}</h3>
+			<button className="catch-btn">CATCH</button>
+		</section>
+
+		<section className="pokedex">
+			<h2>Pokedex</h2>
+			<div className="pokedex-list">
+				{pokedex.map(pokemon => (
+					<div className="pokemon" key={pokemon.id}>
+						<img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + wildPokemon.id + ".png"} className="sprite"/>
+						<h3 className="pokemon-name">{pokemon.name}</h3>
+						<button className="remove">&times;</button>
+				    </div>
+				))}
+			</div>
+		</section>
+	</div>
   )
 }
+
+ReactDom.render(<App />, document.getElementById('root'));
